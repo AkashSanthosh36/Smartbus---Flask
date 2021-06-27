35eda67d4661ec1cb2c41e1baba9d1292abe4c10
Akash S <70521282+AkashSanthosh36@users.noreply.github.com>
2021-06-27 18:04:50 +0530

    Add files via upload

---
+++ app/models.py
@@ -0,0 +1,32 @@
+from app import db,login
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash,check_password_hash
+
+class User(UserMixin,db.Model):
+	id=db.Column(db.Integer,primary_key=True)
+	username=db.Column(db.String(64),index=True,unique=True)
+	email=db.Column(db.String(120),index=True,unique=True)
+	password_hash=db.Column(db.String(128))
+
+	def __repr__(self):
+		return '<User {}>'.format(self.username)
+
+	def set_password(self,password):
+		self.password_hash=generate_password_hash(password)
+
+	def check_password(self,password):
+		return check_password_hash(self.password_hash,password)	
+
+class Bus(db.Model):
+	bid=db.Column(db.Integer,primary_key=True)
+	tname=db.Column(db.String(100),index=True)
+	max_seats=db.Column(db.Integer)
+	type_ac=db.Column(db.String(10))
+	type_sleeper=db.Column(db.String(10))
+
+	def __repr__(self):
+		return '<Bus {}>'.format(Bus.tname)		
+
+@login.user_loader
+def load_user(id):	
+	return User.query.get(int(id)) 			
